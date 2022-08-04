2420ab0659d5b14a3cad111774965b2a8066eb8f
김성수 <fereverv@openbase.co.kr>
2022-08-04 17:13:17 +0900

    html 추가

---
+++ src/main/java/com/jpabook/jpashop/controller/MemberForm.java
@@ -0,0 +1,21 @@
+package com.jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+
+@Getter
+@Setter
+public class MemberForm {
+
+    @NotNull(message = "회원 이름은 필수 입니다.")
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+
+}
